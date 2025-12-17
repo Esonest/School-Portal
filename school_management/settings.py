5705006cfcb7d1c4397ace0f307f9e1046feece7
Ernest Osinachi <osseyernest@gmail.com>
2025-12-17 09:22:03 -0800

    Adjust

--- school_management/settings.py
+++ school_management/settings.py
@@ -40,7 +40,11 @@ MESSAGE_TAGS = {
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ["techcenter-p2au.onrender.com"]  # replace with your Render domain
+ALLOWED_HOSTS = [
+    "techcenter-p2au.onrender.com",
+     "localhost",
+     "127.0.0.1",
+]  # replace with your Render domain
 
 
 
