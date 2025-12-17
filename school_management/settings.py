c960d70a390771bf7905e9a6672ddcba92879f81
Ernest Osinachi <osseyernest@gmail.com>
2025-12-17 09:15:30 -0800

    Adjust

--- school_management/settings.py
+++ school_management/settings.py
@@ -132,10 +132,8 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT', '5432'),
-        'OPTIONS': {
-            'sslmode': 'require',
-            "CONN_MAX_AGE": 600,
-        }
+        'OPTIONS': {'sslmode': 'require'},
+        'CONN_MAX_AGE': 600,
     }
 }
 
