95dbd147e8c52ded356fb01b23e5a16f84743d5b
Ernest Osinachi <osseyernest@gmail.com>
2026-06-27 13:41:04 -0700

    assignment

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,23 +1,32 @@
-# accounts/backends.py
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class ActiveUserBackend(ModelBackend):
-    """
-    Custom backend: only authenticate users who are not deleted and active
-    """
+
     def authenticate(self, request, username=None, password=None, **kwargs):
+
         try:
             user = User.objects.get(username=username)
+
         except User.DoesNotExist:
             return None
 
-        # ❌ Block deleted or inactive users immediately
+        except User.MultipleObjectsReturned:
+            # Prevent server crash
+            user = User.objects.filter(
+                username=username
+            ).order_by("id").first()
+
+            if not user:
+                return None
+
         if user.is_deleted or not user.is_active:
             return None
 
         if user.check_password(password):
             return user
+
         return None
